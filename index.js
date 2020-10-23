fe36d04a352565af93882a1853e3340f6f3d6058
Takahiro Ueda <tkhr6022@gmail.com>
2020-10-23 09:59:45 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById("openModal").addEventListener("click",() => {
+//   document.getElementById("modal").classList.add("active");
+//   document.getElementById("mask").classList.add("active");
+
+// });
+// document.getElementById("closeModal").addEventListener("click",() => {
+//   document.getElementById("modal").classList.remove("active");
+//   document.getElementById("mask").classList.remove("active");
+
+// });
+
+
+console.log("foo");
